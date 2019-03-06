0fc997714409c15fec32956c9bbf0b9b97481949
AlejandraValentina <48266009+AlejandraValentina@users.noreply.github.com>
2019-03-06 07:36:47 -0300

    Initial commit

---
+++ NutriBox/scripts/application.ts
@@ -0,0 +1,29 @@
+﻿// Si quiere una introducción sobre la plantilla En blanco, vea la siguiente documentación:
+// http://go.microsoft.com/fwlink/?LinkID=397705
+// Para depurar código al cargar la página en cordova-simulate o en dispositivos o emuladores Android: inicie la aplicación, establezca puntos de interrupción 
+// y ejecute "window.location.reload()" en la Consola de JavaScript.
+"use strict";
+
+export function initialize(): void {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
+
+function onDeviceReady(): void {
+    document.addEventListener('pause', onPause, false);
+    document.addEventListener('resume', onResume, false);
+
+    // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
+    var parentElement = document.getElementById('deviceready');
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+}
+
+function onPause(): void {
+    // TODO: esta aplicación se ha suspendido. Guarde el estado de la aplicación aquí.
+}
+
+function onResume(): void {
+    // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
+}
---
+++ NutriBox/scripts/startup.ts
@@ -0,0 +1,9 @@
+﻿import * as Application from './application';
+
+declare var require: (modules: string[], ready: Function, errback: Function) => void;
+
+// Pruebe a cargar código específico de la plataforma desde la carpeta /merges.
+// Más información en http://taco.visualstudio.com/es-es/docs/configure-app/#Content.
+require(["./platformOverrides"],
+    () => Application.initialize(),
+    () => Application.initialize());
